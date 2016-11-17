3ef17ab9b5906a312acacffdf75b239021062cd6
Matthieu Oscuro <Matthieu@with.local>
2016-11-17 11:33:33 +0100

    Adding comments to workout related models

--- workouts/models.py
+++ workouts/models.py
@@ -9,6 +9,12 @@ class Workout(models.Model):
     creator = models.ForeignKey('auth.User')
     
 class Step(models.Model):
+    """
+    A Step describes the smallest part of a Workout
+    Exemples :
+        - 15 pullups
+        - 10 bench press @ 15Kg
+    """
     workout = models.ForeignKey(Workout, on_delete=models.CASCADE)
     round = models.IntegerField()
     numero = models.IntegerField()
@@ -16,6 +22,25 @@ class Step(models.Model):
     weight = models.FloatField()
     
 class Session(models.Model):
+    """
+    - A Session materialize the execution of a workout.
+    - It can be seen as a pool where one or more athletes wait for the workout to start
+    - A session can be planned hours or days before the beginning of the workout,
+        in order to wait for other athlete than the creator to join in
+    """
     workout = models.ForeignKey(Workout, on_delete=models.CASCADE)
     creator = models.ForeignKey('auth.User')
-    date
+    date = models.DateTimeField()
+    users = models.ManyToManyField('auth.User')
+    
+class Progression(models.Model):
+    """
+    - The Progression follows where an athlete is at during a workout
+    - It logs the time the athlete took to complete each step of a workout
+    - The Progression will be used to update every athlete's leaderboard during a workout in "competition mode"
+    """
+    session = models.ForeignKey(Session)
+    step = models.ForeignKey(Step)
+    user = models.ForeignKey('auth.User')
+    time = models.IntegerField() # seconds
+    
