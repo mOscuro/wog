377af4f32b1fad5b33b3fa6c2d2760c802f3f34f
matthieu oscuro <matthieu.oscuro@gmail.com>
2017-10-05 20:06:23 +0200

    manage DJANGO_DEFAULT_SETTINGS the right way

--- wogether/wsgi.py
+++ wogether/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("WOG_DJANGO_SETTINGS_MODULE", "wogether.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wogether.settings")
 
 application = get_wsgi_application()
